2330879368be82afbb51c1bee544d61c31ca7c8b
Fezalion <raincloud@hotmail.com.tr>
2020-09-07 15:35:13 +0300

    more modules

---
+++ Modules/Moderation/GuildSettings.cs
@@ -0,0 +1,68 @@
+﻿using System.Linq;
+using System.Threading.Tasks;
+using Discord.Commands;
+using Discord.WebSocket;
+using FezBotRedux.Common.Attributes;
+using FezBotRedux.Common.Enums;
+using FezBotRedux.Common.Extensions;
+using FezBotRedux.Common.Models;
+
+namespace FezBotRedux.Modules.GuildSettings
+{
+    [Name("Guild"), Summary("contains the guild settings commands.")]
+    public class GuildSettings : ModuleBase<SocketCommandContext>
+    {
+        [Group("prefix"), Alias("pfx"), Name("Guild Prefix")]
+        public class GuildPrefixSub : ModuleBase<SocketCommandContext>
+        {
+            [Command("")]
+            [Remarks("Sets custom prefix.")]
+            [MinPermissions(AccessLevel.ServerAdmin)]
+            public async Task Prefix([Summary("Prefix")]string prefix)
+            {
+                if (prefix.Length < 0 || prefix.Length > 15)
+                {
+                    var embed = NeoEmbeds.Error("Prefix length can't be less than 0 or bigger than 15.", Context.User);
+                    await ReplyAsync("", false, embed.Build());
+                }
+                else
+                {
+                    using (var db = new NeoContext())
+                    {
+                        var obj = db.Guilds.FirstOrDefault(g => g.Id == Context.Guild.Id);
+                        obj.Prefix = prefix;
+                        db.Update(obj);
+                        db.SaveChanges();
+                        var embed = NeoEmbeds.Success($"Prefix of `{Context.Guild.Name}` has been set to `{prefix}`.", Context.User);
+                        await ReplyAsync("", false, embed.Build());
+                    }
+                }
+            }
+
+            [Command("")]
+            [Remarks("Gets custom prefix of the server.")]
+            [MinPermissions(AccessLevel.User)]
+            public async Task Prefix()
+            {
+                string prefix;
+                using (var db = new NeoContext())
+                {
+                    var obj = db.Guilds.FirstOrDefault(g => g.Id == Context.Guild.Id);
+                    prefix = obj.Prefix;
+                }
+
+                if (prefix == null)
+                {
+                    var embed = NeoEmbeds.Error("This guilds custom prefix is not set yet.", Context.User);
+                    await ReplyAsync("", false, embed.Build());
+                }
+                else
+                {
+                    var embed = NeoEmbeds.Success($"Prefix of `{Context.Guild.Name}` is `{prefix}`", Context.User);
+                    await ReplyAsync("", false, embed.Build());
+                }
+            }
+        }
+    }
+}
+
---
+++ Modules/Moderation/ModeratorModule.cs
@@ -0,0 +1,76 @@
+﻿using System.Linq;
+using System.Threading.Tasks;
+using Discord;
+using Discord.Commands;
+using Discord.WebSocket;
+using FezBotRedux.Common.Attributes;
+using FezBotRedux.Common.Enums;
+using FezBotRedux.Common.Extensions;
+
+namespace FezBotRedux.Modules.Moderation
+{
+    [Name("Moderator"), Summary("contains moderating commands for server mods.")]
+    [RequireContext(ContextType.Guild)]
+    [MinPermissions(AccessLevel.ServerMod)]
+    public class ModeratorModule : ModuleBase<SocketCommandContext>
+    {
+        [Command("kick")]
+        [Remarks("Kick the specified user.")]
+        [MinPermissions(AccessLevel.ServerMod)]
+        public async Task Kick([Summary("User Mention")][Remainder]SocketGuildUser user)
+        {
+            await ReplyAsync($"cya {user.Mention} :wave:");
+            await user.KickAsync();
+        }
+
+        [Command("ban")]
+        [Remarks("Ban the specified user.")]
+        [MinPermissions(AccessLevel.ServerMod)]
+        public async Task Ban([Summary("User Mention")][Remainder]SocketGuildUser user)
+        {
+            await ReplyAsync($"There won't be a next time... :cry: {user.Username} :wave:");
+            await Context.Guild.AddBanAsync(user);
+        }
+
+        [Command("prune")]
+        [Remarks("Clear bots recent messages.")]
+        [MinPermissions(AccessLevel.ServerMod)]
+        public async Task Clearm()
+        {
+            var self = Context.Client.CurrentUser;
+
+            var messages = (await Context.Channel.GetMessagesAsync().FlattenAsync()).AsEnumerable();
+            messages = messages.Where(x => x.Author.Id == self.Id);
+            //TODO await Context.Channel.DeleteMessagesAsync(messages);
+
+            var embed = NeoEmbeds.Success($"Deleted {messages.Count()} messages.", Context.User);
+            await ReplyAsync("", false, embed.Build());
+        }
+
+        [Command("prune")]
+        [Remarks("Clear a users recent messages.")]
+        [MinPermissions(AccessLevel.ServerMod)]
+        public async Task Clearm([Summary("User Mention")]IUser user)
+        {
+            var messages = (await Context.Channel.GetMessagesAsync().FlattenAsync()).AsEnumerable();
+            messages = messages.Where(x => x.Author.Id == user.Id);
+            //TODO await Context.Channel.DeleteMessagesAsync(messages);
+
+            var embed = NeoEmbeds.Success($"Deleted {messages.Count()} messages of {user.Username}.", Context.User);
+            await ReplyAsync("", false, embed.Build());
+        }
+
+        [Command("prune")]
+        [Remarks("Clear a users recent messages.")]
+        [MinPermissions(AccessLevel.ServerMod)]
+        public async Task Clearm([Summary("User Mention")]IUser user, [Summary("Message Amount")] int amount)
+        {
+            var messages = (await Context.Channel.GetMessagesAsync(amount < 100 ? amount : 100).FlattenAsync()).AsEnumerable();
+            messages = messages.Where(x => x.Author.Id == user.Id);
+            //TODO await Context.Channel.DeleteMessagesAsync(messages);
+
+            var embed = NeoEmbeds.Success($"Deleted {messages.Count()} messages of {user.Username}.", Context.User);
+            await ReplyAsync("", false, embed.Build());
+        }
+    }
+}
